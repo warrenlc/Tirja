723704fbbd4bec50838d8989d8d7f989bc169323
warrenlc <warrencrutcher@gmail.com>
2023-06-30 23:03:01 +0200

    Considering doing this in C++...we'll see

--- cpp/Tokenizer.h
+++ cpp/Tokenizer.h
@@ -5,27 +5,74 @@
 #ifndef TIRJA_TOKENIZER_H
 #define TIRJA_TOKENIZER_H
 
-enum class State {
-    START,
+enum class Token_Type {
     SEMICOLON,
     LPAREN,
     RPAREN,
-    OPERATION,
+    INCREMENT,
+    DECREMENT,
+    MINUS,
+    BWNOT,
+    POWER,
+    TIMES,
+    DIVIDE,
+    MOD,
+    PLUS,
+    BWLEFT,
+    BWRIGHT,
+    BWAND,
+    XOR,
+    NOT,
+    AND,
+    OR,
+    LESS_THAN,
+    GREATER_THAN,
+    EQUIVALENT,
+    NOT_EQUIVALENT,
+    LESS_THAN_EQ,
+    GREATER_THAN_EQ,
+    TRUE,
+    FALSE,
     NUMBER,
-    IDENTIFIER,
-    NEWLINE,
-    END
+    NAME,
+    EQUALS,
+    IF,
+    DO,
+    FOR,
+    ELSE,
+    THEN,
+    WHILE,
+    NOTHING
 };
 
-struct Token {
-    State state;
-    std::string lexeme;
+class Token {
+    public:
+        Token(std::string lexeme, Token_Type type)
+        : lexeme{lexeme}
+        , type{type} {}
+    
+    protected:
+        std::string lexeme;
+        Token_Type  type;
 };
 
 
 class Tokenizer {
+    protected:
+        Tokenizer(std::string string_input)
+        : string_input{string_input} {}
+
+        static Tokenizer* tokenizer;
+
+        std::string string_input;
+    
     public:
-        Tokenizer(){};
+        Tokenizer(Tokenizer &other_tokenizer) = delete;
+        void operator=(const Tokenizer &)     = delete;
+
+        static Tokenizer *get_instance(const std::string &input_string);
+
+        std::string to_string() const;
 };
 
 
